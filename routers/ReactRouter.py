1913c9b6a0e4364bcb8091fe9242a8824c728da7
leemhoon00 <leemhoon000@gmail.com>
2023-08-10 13:03:49 +0000

    feat: ReactRouter

--- routers/ReactRouter.py
+++ routers/ReactRouter.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
-from pydantic import BaseModel
-import boto3
+from fastapi import Request
 import os
 from dotenv import load_dotenv
+import pymysql
+import boto3
 import requests
 
 load_dotenv()
@@ -10,18 +11,28 @@ load_dotenv()
 IPV4_SET_NAME=os.environ['IPV4_SET_NAME'].strip()
 IPV4_SET_ID=os.environ['IPV4_SET_ID'].strip()
 
-LambdaRouter = APIRouter(
-    prefix="/lambda"
+ReactRouter = APIRouter(
+    prefix="/react"
 )
 
-class Item(BaseModel):
-    ip: str
-
-@LambdaRouter.post('/')
-async def post_lambda(item: Item):
+@ReactRouter.get('/')
+async def ban(request: Request):
+    con = pymysql.connect(
+        host = os.environ['DATABASE_HOST'].strip(),
+        user = os.environ['DATABASE_USER'].strip(),
+        password = os.environ['DATABASE_PASSWORD'].strip(),
+        db = os.environ['DATABASE_DB'].strip(),
+        charset='utf8mb4'
+    )
     try:
-        client = boto3.client('wafv2', region_name = 'ap-northeast-2')
+        cur = con.cursor()
+        sql = "insert into ban_list values('"+request.client.host+"', '4', current_timestamp)"
+        cur.execute(sql)
+        con.commit()
+        cur.close()
+        con.close()
 
+        client = boto3.client('wafv2', region_name = 'ap-northeast-2')
         response = client.get_ip_set(
             Name=IPV4_SET_NAME,
             Scope='REGIONAL',
@@ -29,8 +40,9 @@ async def post_lambda(item: Item):
         )
 
         Address_list = response["IPSet"]["Addresses"]
-        Address_list.append(item.ip)
+        Address_list.append(request.client.host+"/32")
         lock_token = response['LockToken']
+
         response = client.update_ip_set(
             Name = IPV4_SET_NAME,
             Scope = 'REGIONAL',
@@ -39,7 +51,6 @@ async def post_lambda(item: Item):
             Addresses = Address_list,
             LockToken=lock_token
         )
-
         template = {
             "blocks": [
                 {
@@ -56,7 +67,7 @@ async def post_lambda(item: Item):
                     "type": "section",
                     "text": {
                         "type": "plain_text",
-                        "text": item.ip
+                        "text": request.client.host
                     }
                 }
             ]
@@ -73,8 +84,7 @@ async def post_lambda(item: Item):
         }
 
         response = requests.post(url, headers=headers, json=payload)
-    
-    except:
-        return "에러"
+        return "OK"
 
-    return
+    except:
+        return "ERROR"
